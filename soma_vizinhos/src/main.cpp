10b28fc4fd1488ecac788c5cf4dd799e4ca57f0a
Ana Caroline Dantas <caroline.dantas.016@ufrn.edu.br>
2021-06-14 23:11:07 +0000

    Implementando Soma Vizinhos

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -4,13 +4,21 @@
  * @data June, 6th 2021
  */
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
+#include <stdlib.h>
+using namespace std;
 
 int main( void )
 {
-    // TODO: Adicione seu código aqui. 
-
+    int m, n, soma, x;
+	while( cin >> std::ws >> m >> n) {
+		soma = m;
+		for (int i = 0; i < abs(n)-1; i++) {
+			if (n > 0)
+				soma += ++m;
+			else   
+				soma += --m;
+		}
+        cout<<soma<<endl;
+	}    
     return 0;
 }
