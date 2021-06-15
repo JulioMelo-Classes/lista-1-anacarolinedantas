4e5e2bdf6a5f0f28213515a7a76fd38a23899f57
Ana Caroline Dantas <caroline.dantas.016@ufrn.edu.br>
2021-06-15 19:14:30 +0000

    Implementando Fibonacci

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -2,15 +2,14 @@
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-	// std::vector<unsigned int> fib = {};
-	// if (n > 1) {
-	// 	fib = {1, 1};
-	// 	int soma = fib[0] + fib[1];
-	// 	while (soma < n) {
-	// 		fib.push_back(soma);
-	// 		soma = fib[fib.size()-1] + fib[fib.size()-2];
-	// 	}
-	// }
-	// return fib;
-    return std::vector<unsigned int>{};
+	std::vector<unsigned int> fib = {};
+	if (n > 1) {
+		fib = {1, 1};
+		int soma = fib[0] + fib[1];
+		while (soma < n) {
+			fib.push_back(soma);
+			soma = fib[fib.size()-1] + fib[fib.size()-2];
+		}
+	}
+	return fib;
 }
